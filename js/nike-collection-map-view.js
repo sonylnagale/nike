9d4b0abf56e6a56d588c2802b27295fbc998d15d
Ryan Cheung <ryanc@livefyre.com>
2014-02-12 18:08:38 -0800

    Add notify animation

--- js/nike-collection-map-view.js
+++ js/nike-collection-map-view.js
@@ -6,8 +6,27 @@ var inherits = Livefyre.require('inherits');
 
 var NikeCollectionMapView = function (opts) {
     CollectionMapView.call(this, opts);
+
+    this._collectionToMarker = {};
 };
 inherits(NikeCollectionMapView, CollectionMapView);
+
+NikeCollectionMapView.prototype.notify = function (collection) {
+    var marker = this._collectionToMarker[collection.id];
+    if (!marker) {
+        return;
+    }
+
+    var ring1 = d3.select($(marker._icon).find('.hub-map-heat-marker-1')[0]);
+    var ring2 = d3.select($(marker._icon).find('.hub-map-heat-marker-2')[0]);
+    var ring3 = d3.select($(marker._icon).find('.hub-map-heat-marker-3')[0]);
+    var ring4 = d3.select($(marker._icon).find('.hub-map-heat-marker-4')[0]);
+
+    ring1.attr('stroke-opacity', '0.6').transition().duration(1000).attr('stroke-opacity', '0');
+    ring2.transition().delay(50).attr('stroke-opacity', '0.35').transition().duration(1000).attr('stroke-opacity', '0');
+    ring3.transition().delay(200).attr('stroke-opacity', '0.15').transition().duration(1000).attr('stroke-opacity', '0');
+    ring4.transition().delay(300).attr('stroke-opacity', '0.05').transition().duration(1000).attr('stroke-opacity', '0');
+};
 
 NikeCollectionMapView.prototype._drawMap = function () {
     this._map = new L.Map(this.el, this._leafletMapOptions).setView(
@@ -44,6 +63,7 @@ NikeCollectionMapView.prototype._drawMap = function () {
 NikeCollectionMapView.prototype._drawMarker = function (dataPoint) {
     var collection = dataPoint.getCollection();
     var collectionArchive = collection.createArchive();
+
     collectionArchive.once('data', function (data) {
         var thumbnailUrl;
         var contentItem = data;
@@ -52,7 +72,13 @@ NikeCollectionMapView.prototype._drawMarker = function (dataPoint) {
         } else if (contentItem.author && contentItem.author.avatar) {
             thumbnailUrl = contentItem.author.avatar;
         }
-        console.log(thumbnailUrl);
+
+        var svgRings = '<svg class="hub-map-heat-marker" width="305" height="305">';
+        svgRings += '<circle class="hub-map-heat-marker-1" cx="152.5" cy="152.5" r="42.5" stroke="#fd5300" stroke-width="15" stroke-opacity="0" fill="none"></circle>';
+        svgRings += '<circle class="hub-map-heat-marker-2" cx="152.5" cy="152.5" r="80" stroke="#fd5300" stroke-width="15" stroke-opacity="0" fill="none"></circle>'
+        svgRings += '<circle class="hub-map-heat-marker-3" cx="152.5" cy="152.5" r="117.5" stroke="#fd5300" stroke-width="15" stroke-opacity="0" fill="none"></circle>'
+        svgRings += '<circle class="hub-map-heat-marker-4" cx="152.5" cy="152.5" r="145" stroke="#fd5300" stroke-width="15" stroke-opacity="0" fill="none"></circle>'
+        svgRings += '</svg>';
 
         var latlng = this._getLatLngFromPoint(dataPoint);
         var marker = new L.Marker(
@@ -61,14 +87,16 @@ NikeCollectionMapView.prototype._drawMarker = function (dataPoint) {
                     className: 'hub-map-content-marker',
                     html: MarkerTemplate({
                         thumbnail_url: thumbnailUrl || ''
-                    }),
-                    iconSize: [44,48],
-                    //iconAnchor: [22,48],
+                    }) + svgRings,
+                    iconSize: [53,53],
                     content: contentItem
                 })
             }
         );
 
         this._addMarkerToMap(marker);
+        this._collectionToMarker[collection.id] = marker;
+
+        //if (collection.id == 48078130) setTimeout(function () { setInterval(function () {this.notify(collection); }.bind(this), 2000) }.bind(this), 2000);
     }.bind(this));
 };
